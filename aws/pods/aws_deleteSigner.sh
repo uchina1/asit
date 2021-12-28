6c9d832d3298147415324242cc51aa426e3bc430
uchina1 <uchina1oreryu@gmail.com>
2021-12-28 20:07:55 +0900

    構成変更

--- aws/pods/aws_deleteSigner.sh
+++ aws/pods/aws_deleteSigner.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-kubectl delete -f Signer/Deployments/aws_signer-replica.yaml 
-kubectl delete -f Signer/Deployments/aws_attacker-replica.yaml
+kubectl delete -f Signer/Deployments/aws_signer-replica.yaml --force
+kubectl delete -f Signer/Deployments/aws_attacker-replica.yaml --force
